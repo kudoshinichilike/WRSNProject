ded80e8ad59f308e0d7a0c78ece65950c8533363
Phuong <kudoshinichilike@gmail.com>
2021-05-02 09:21:21 +0700

    print log

--- Test.py
+++ Test.py
@@ -15,8 +15,8 @@ import sys
 
 read_file = "thaydoisotarget"
 write_file = "try"
-data_range = 1
-data_start = 2
+data_range = 5
+data_start = 0
 run_range = 1
 learning_rate = 0.1
 scale_factor = 0.9
@@ -48,7 +48,7 @@ except:
 #     max_time = None
 
 df = pd.read_csv(read_name)
-for id_data in range(data_range):
+for id_data in range(4,data_range):
     index = id_data + data_start
     print("nb data = ", index)
     write_name = "log/" + write_file + str(index) + ".csv"
